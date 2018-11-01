ee210e8a6207f36ab14a13d2616283538d59f714
jagugo2 <jan-gunthergosselke@gmx.de>
2018-11-01 16:27:22 +0100

    Current Status

--- descriptive.py
+++ descriptive.py
@@ -8,11 +8,8 @@ Created on Wed Oct 31 11:17:24 2018
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-<<<<<<< HEAD
 fontsize = 20
 
-=======
->>>>>>> 46285951fc36009db5dd11b2cb328dfb7ff6d227
 def temp_descriptive(data):
     months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     summary = data.describe()
@@ -21,10 +18,6 @@ def temp_descriptive(data):
         for i in range(1,13):
             subset = data.loc[(data['City'] == city) & (data.Date.dt.month == i)]['Mean Temperature Actual']
             per_month.append(subset)
-<<<<<<< HEAD
-=======
-        fontsize = 20
->>>>>>> 46285951fc36009db5dd11b2cb328dfb7ff6d227
         fig, ax = plt.subplots(figsize=(25,10))
         sns.boxplot(data=per_month);
         sns.despine()
@@ -32,6 +25,8 @@ def temp_descriptive(data):
         ax.set_title("Daily Mean Temperature by Month - " + city, fontsize=fontsize)
         ax.set_xticklabels(months)
         ax.set_ylabel("Degree Celcius",fontsize=fontsize);
+        fig.savefig('./plots/' + city + '_boxplots')
+        
         
         fig, axes = plt.subplots(nrows=4, ncols=3,
                          sharex=True, sharey=True,
@@ -43,8 +38,7 @@ def temp_descriptive(data):
             sns.distplot(subset['Mean Temperature Actual'], kde=True, ax=ax)
             ax.set_title(months[month_i])
             # Save the full figure...
-<<<<<<< HEAD
-            fig.savefig('./plots/' + city + 'monthly_temperatures.png')
+            fig.savefig('./plots/' + city + '_monthly_temperatures.png')
     return summary
 
 def return_histogram(data, index):
@@ -54,10 +48,4 @@ def return_histogram(data, index):
     ax.set_ylabel("Frequency",fontsize=fontsize)
     sns.distplot(data)
     fig.savefig('./plots/' + index + '_hist.png')
-=======
-            fig.savefig('./plots/monthly_temperatures.png')
-    return summary
 
-def return_histogram(data):
-    sns.distplot(data)
->>>>>>> 46285951fc36009db5dd11b2cb328dfb7ff6d227
---
+++ loading_data.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[40]:
+
+
+import sys
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from datetime import datetime
+
+# ignore warnings because they are distracting 
+import warnings
+warnings.filterwarnings('ignore')
+
+# suppress the scientific notation when printing numpy arrays
+np.set_printoptions(suppress=True)
+
+dateparse = lambda x: pd.datetime.strptime(x, '%d/%m/%Y')
+# # Loading the financial data
+
+# In[83]:
+def load_data(stock_path, weather_path):
+    stocks = pd.read_csv(stock_path,
+                 sep=';',
+                 parse_dates=['Date'],
+                 date_parser=dateparse,
+                 decimal=',')
+
+    # data cleansing
+    stocks['Index'] = [name.replace(".", "") for name in stocks['Index'].values]
+    data_per_index = {}
+ 
+    # # Loading the weather data
+
+    weather_per_city = pd.read_csv(weather_path,
+                 sep=';',
+                 parse_dates=['Date'],
+                 date_parser=dateparse,
+                 decimal=',')
+    cities = pd.DataFrame({'City': ["New York", "Boston", "San Francisco", "Chicago", "London", "Zurich", "Tokyo"],
+                              'Country': ["USA", "USA", "USA", "USA", "UK", "Switzerland", "Japan"]})
+    weather_per_city = pd.merge(weather_per_city, cities, on="City")
+    # look at how many NaNs we have
+    # df.isna().sum()
+    # drop NaNs
+    weather_per_city.dropna(inplace=True)
+    
+    for stock_index in stocks.Index.unique():
+        data_per_index[stock_index] = pd.merge(stocks[stocks["Index"] == stock_index], weather_per_city, on="Date")
+    # show all cities
+    print([city for city in weather_per_city['City'].unique()])
+    # assumes (nrows x ncols) episodes
+    return data_per_index, weather_per_city
+
